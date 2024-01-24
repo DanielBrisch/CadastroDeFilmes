bf2dd0b386e6b3e9ac48bab831cadcbd5d68c9e7
DanielBrisch <danielbrisch157@gmail.com>
2024-01-24 00:29:07 -0300

    commit sem o botão de idade estar finalizado

--- projetofilmes/lib/Screens/movies_register/mv_register_page.dart
+++ projetofilmes/lib/Screens/movies_register/mv_register_page.dart
@@ -13,8 +13,17 @@ class MV_RegisterPage extends StatefulWidget {
 class _MV_RegisterPageState extends State<MV_RegisterPage> {
   @override
   Widget build(BuildContext context) {
-
     Size size = MediaQuery.of(context).size;
+
+    List<bool> selectedYears = <bool>[false, false, false, false, false];
+
+    const List<Widget> idades = <Widget>[
+      Text('10'),
+      Text('12'),
+      Text('14'),
+      Text('16'),
+      Text('18'),
+    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -30,7 +39,7 @@ class _MV_RegisterPageState extends State<MV_RegisterPage> {
       body: SingleChildScrollView(
         child: Container(
           width: size.width,
-          height: size.height,
+          height: 930,
           color: Colors.white,
           child: Column(
             children: [
@@ -163,79 +172,60 @@ class _MV_RegisterPageState extends State<MV_RegisterPage> {
                       ),
                       SizedBox(
                         width: 340,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            fillColor: Colors.white,
-                            filled: true,
-                            hintText: "Insira a descrição do filme",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              borderSide: const BorderSide(
-                                  color: Colors.deepPurpleAccent),
+                        child: Container(
+                          height: 150,
+                          child: TextField(
+                            maxLines: null,
+                            expands: true,
+                            decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              hintText: "Insira a descrição do filme",
+                              alignLabelWithHint: false,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: const BorderSide(
+                                  color: Colors.deepPurpleAccent,
+                                ),
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ],
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        FloatingActionButton(
-                          onPressed: () {},
-                          backgroundColor: Colors.lightBlueAccent,
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: const Text(
-                            '10',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        FloatingActionButton(
-                          onPressed: () {},
-                          backgroundColor: Colors.yellow,
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: const Text(
-                            '12',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        FloatingActionButton(
-                          onPressed: () {},
-                          backgroundColor: Colors.red,
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: const Text(
-                            '16',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        FloatingActionButton(
-                          onPressed: () {},
-                          backgroundColor: Colors.black,
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: const Text(
-                            '18',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ],
-                    ),
+                  const SizedBox(height: 20),
+                    //nao funciona ainda (mas deveria
+                  ToggleButtons(
+                    isSelected: selectedYears,
+                    onPressed: (int index) {
+                      setState(() {
+                        for (int buttonIndex = 0; buttonIndex < selectedYears.length; buttonIndex++) {
+                          if (buttonIndex == index) {
+                            selectedYears[buttonIndex] = true;
+                          } else {
+                            selectedYears[buttonIndex] = false;
+                          }
+                        }
+                      });
+                    },
+                    children: idades
                   ),
-                  FloatingActionButton(
-                    onPressed: () {},
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: SizedBox(
+                      width: 150,
+                      height: 50,
+                      child: FloatingActionButton(
+                        backgroundColor: Colors.deepPurpleAccent,
+                        onPressed: () {},
+                        child: const Text(
+                          'SALVAR',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
--- projetofilmes/lib/Screens/movies_register/mv_register_store.dart
+++ projetofilmes/lib/Screens/movies_register/mv_register_store.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
 class MV_RegisterStore {
   static int star = 0;
+
+  static final List<bool> _idadesSelecionadas = <bool>[false, false, false];
+
+
 }
